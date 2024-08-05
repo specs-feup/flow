ba8aa82fd5ed5898b646b639de3f1c66bd1c9ed9
L. Sousa <lm.sousa@fe.up.pt>
2024-08-05 18:41:24 +0100

    Adds documentation generation to repo

---
+++ typedoc.config.js
@@ -0,0 +1,7 @@
+import { fileURLToPath } from "url";
+
+export default {
+  extends: [ fileURLToPath(import.meta.resolve("lara-js/typedoc.base.json")) ],
+  entryPoints: ["src/"],
+  tsconfig: "tsconfig.json",
+}
